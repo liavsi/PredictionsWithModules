d7f6cf1ae0396e3b043cbe6f759cb23f226712ea
Liav Simantov <liavsi@mta.ac.il>
2023-08-16 22:55:26 +0300

    added ui

---
+++ ConsoleUserInterface/src/userinterface/UI.java
@@ -0,0 +1,44 @@
+package userinterface;
+
+import engine.SimulationOutcome;
+import engine.impl.EngineImpl;
+
+import java.util.Scanner;
+
+public class UI {
+    private static final String JAXB_XML_PACKAGE_NAME = "schema.generated";
+    private static final String XML_FILE_PATH = "resources/master-ex1.xml";
+    public static void main(String[] args) {
+        EngineImpl currEngine = new EngineImpl();
+        System.out.println("please enter your choice:");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+//        switch (choice) {
+//            case 1:
+//                currEngine.readWorldFromXml(XML_FILE_PATH, JAXB_XML_PACKAGE_NAME);
+//                break;
+//            case 2:
+//                showDataToUser(currEngine.getWorldDTO());
+//                break;
+//            case 3:
+//                showSimulationIdNumber(currEngine.runNewSimulation());
+//                break;
+//            case 4:
+//                int wantedSimulationNumber = getSimulationNumberFromUser();
+//                showSimulationData(currEngine.getPastSimulationDTO(wantedSimulationNumber));
+//                break;
+//            case 5:
+//                System.exit(1);
+//                break;
+//            default:
+//                throw new IllegalArgumentException(choice + "is not a valid choice");
+//        }
+        currEngine.readWorldFromXml(XML_FILE_PATH, JAXB_XML_PACKAGE_NAME);
+        System.out.println(currEngine.toString());
+    }
+
+    private static void showSimulationData(SimulationOutcome newSimulation) {
+
+    }
+
+}
