7819f05a41896d0805fc9b3b62969e29b59f0b7e
Liav Simantov <liavsi@mta.ac.il>
2023-08-28 00:40:46 +0300

    build the skeleton of the program - header and details

--- JavaFxUserInterface/src/component/mainapp/TasksMain.java
+++ JavaFxUserInterface/src/component/mainapp/TasksMain.java
@@ -1,31 +1,40 @@
 package component.mainapp;
 
+import engine.api.Engine;
+import engine.impl.EngineImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class TasksMain extends Application {
 
+    private static final String PRIMARY_STAGE_NAME ="Prediction app";
+    private static String APP_FXML_INCLUDE_RESOURCE = "mainApp.fxml";
     @Override
     public void start(Stage primaryStage) {
-
         // CSSFX.start();
-
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL url = getClass().getResource(APP_FXML_INCLUDE_RESOURCE);
+        fxmlLoader.setLocation(url);
         Parent root = null;
+        // give the controllers access to the engine
+        AppController controller = fxmlLoader.getController();
+        controller.setEngine(new EngineImpl());
         try {
-            root = FXMLLoader.load(getClass().getResource("/component/header/headerView.fxml"));
+            root = fxmlLoader.load(url.openStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        Scene scene = new Scene(root, 800, 600);
-
+        Scene scene = new Scene(root, 500, 600);
         // Set the Scene and show the Stage
-        primaryStage.setTitle("JavaFX App with Scene Builder");
+        primaryStage.setTitle(PRIMARY_STAGE_NAME);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
