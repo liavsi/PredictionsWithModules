aeeae6d1b7aa3d933f82cb76646a557fbe956aeb
shani384 <118974759+shani384@users.noreply.github.com>
2023-09-21 01:31:17 +0300

    debug

--- Engine/src/engine/impl/RunSimulation.java
+++ Engine/src/engine/impl/RunSimulation.java
@@ -9,6 +9,7 @@ import engine.world.design.execution.context.ContextImpl;
 import engine.world.design.execution.entity.api.EntityInstance;
 import engine.world.design.execution.property.PropertyInstance;
 import engine.world.design.execution.property.PropertyInstanceImpl;
+import engine.world.design.expression.Expression;
 import engine.world.design.rule.Rule;
 import engine.world.design.termination.api.Termination;
 import engine.world.design.world.api.World;
@@ -33,79 +34,99 @@ public class RunSimulation extends Thread implements Runnable{
 
     @Override
     public void run() {
-        for (PropertyDefinition envVarDefinition: world.getEnvVariablesManager().getEnvVariables().values()) {
-            String envName = envVarDefinition.getName();
-            if(propertyNameToValueAsString.containsKey(envName)) {
-                Object value = envVarDefinition.getType().convert(propertyNameToValueAsString.get(envName));
-                simulationOutcome.getActiveEnvironment().addPropertyInstance(new PropertyInstanceImpl(envVarDefinition,value));
-            }
-            else {
-                simulationOutcome.getActiveEnvironment().addPropertyInstance(new PropertyInstanceImpl(envVarDefinition, envVarDefinition.generateValue()));
-            }
-        }
-        // showFinalEnvProperties();
-        // creating the instance manager
-        for (EntityDefinition entityDefinition: world.getNameToEntityDefinition().values()) {
-            Double population = (Double) propertyNameToValueAsString.get(entityDefinition.getName()+"entity");
-            entityDefinition.setPopulation(population.intValue());
-            for (int i = 0 ;i < entityDefinition.getPopulation(); i++) {
-                simulationOutcome.getEntityInstanceManager().create(entityDefinition);
-            }
-        }
-        Termination termination = simulationOutcome.getTermination();
-        termination.startTerminationClock();
-        Map<Integer, SimulationOutcome> informationSimulation = new HashMap<>();
-        // take a picture
-        termination.setCurrTick(0);
-        // TODO: 15/09/2023 setTicks
-//        simulationOutcome.getTermination()
-        while (!simulationOutcome.getTermination().isTerminated(simulationOutcome.isStop())) {
-            moveEntities();
-            ArrayList<Action> activeActions = new ArrayList<>();
-            for (Rule rule: world.getRules()) {
-                if (rule.getActivation().isActive(termination.getCurrTick())) {
-                    activeActions.addAll(rule.getActionToPreform());
+        try {
+            for (PropertyDefinition envVarDefinition : world.getEnvVariablesManager().getEnvVariables().values()) {
+                String envName = envVarDefinition.getName();
+                if (propertyNameToValueAsString.containsKey(envName)) {
+                    Object value = envVarDefinition.getType().convert(propertyNameToValueAsString.get(envName));
+                    simulationOutcome.getActiveEnvironment().addPropertyInstance(new PropertyInstanceImpl(envVarDefinition, value));
+                } else {
+                    simulationOutcome.getActiveEnvironment().addPropertyInstance(new PropertyInstanceImpl(envVarDefinition, envVarDefinition.generateValue()));
                 }
             }
-            for (EntityInstance entityInstance: simulationOutcome.getEntityInstanceManager().getInstances().values()) {
-                for (Action action: activeActions){
-                    if (action.getMainEntity().getName().equals(entityInstance.getEntityDefinition().getName())){ //if the action activate on the entity
-                        if (action.getInteractiveEntity() != null) {
-                            Context context = new ContextImpl(null, null, simulationOutcome.getEntityInstanceManager(), simulationOutcome.getActiveEnvironment(), simulationOutcome.getGrid());
-                            for (EntityInstance secondaryEntity : action.getSecondaryInstances(context)) {
-                                context = new ContextImpl(entityInstance, secondaryEntity, simulationOutcome.getEntityInstanceManager(), simulationOutcome.getActiveEnvironment(), simulationOutcome.getGrid());
+            // showFinalEnvProperties();
+            // creating the instance manager
+            for (EntityDefinition entityDefinition : world.getNameToEntityDefinition().values()) {
+                Object value = propertyNameToValueAsString.get(entityDefinition.getName() + "entity");
+                double population = getPopulation(value);
+                entityDefinition.setPopulation((int) population);
+                for (int i = 0; i < entityDefinition.getPopulation(); i++) {
+                    simulationOutcome.getEntityInstanceManager().create(entityDefinition);
+                }
+            }
+            Termination termination = simulationOutcome.getTermination();
+            termination.startTerminationClock();
+            Map<Integer, SimulationOutcome> informationSimulation = new HashMap<>();
+            // take a picture
+            termination.setCurrTick(0);
+            // TODO: 15/09/2023 setTicks
+//        simulationOutcome.getTermination()
+            while (!simulationOutcome.getTermination().isTerminated(simulationOutcome.isStop())) {
+                moveEntities();
+                ArrayList<Action> activeActions = new ArrayList<>();
+                for (Rule rule : world.getRules()) {
+                    if (rule.getActivation().isActive(termination.getCurrTick())) {
+                        activeActions.addAll(rule.getActionToPreform());
+                    }
+                }
+                for (EntityInstance entityInstance : simulationOutcome.getEntityInstanceManager().getInstances().values()) {
+                    for (Action action : activeActions) {
+                        if (action.getMainEntity().getName().equals(entityInstance.getEntityDefinition().getName())) { //if the action activate on the entity
+                            if (action.getInteractiveEntity() != null) {
+                                Context context = new ContextImpl(null, null, simulationOutcome.getEntityInstanceManager(), simulationOutcome.getActiveEnvironment(), simulationOutcome.getGrid());
+                                for (EntityInstance secondaryEntity : action.getSecondaryInstances(context)) {
+                                    context = new ContextImpl(entityInstance, secondaryEntity, simulationOutcome.getEntityInstanceManager(), simulationOutcome.getActiveEnvironment(), simulationOutcome.getGrid());
+                                    action.invoke(context);
+                                }
+                            } else {
+                                Context context = new ContextImpl(entityInstance, null, simulationOutcome.getEntityInstanceManager(), simulationOutcome.getActiveEnvironment(), simulationOutcome.getGrid());
                                 action.invoke(context);
                             }
-                        }else{
-                            Context context = new ContextImpl(entityInstance, null, simulationOutcome.getEntityInstanceManager(), simulationOutcome.getActiveEnvironment(), simulationOutcome.getGrid());
-                            action.invoke(context);
+                        }
+                    }
+                    for (PropertyInstance propertyInstance : entityInstance.getProperties().values()) {
+                        if (propertyInstance.getValue().equals(propertyInstance.getOldValue())) {
+                            propertyInstance.setTicksSameValue(propertyInstance.getTicksSameValue() + 1);
+                        } else {
+                            propertyInstance.setOldValue(propertyInstance.getValue());
+                            propertyInstance.setTicksSameValue(0);
                         }
                     }
                 }
-                for (PropertyInstance propertyInstance:entityInstance.getProperties().values()){
-                    if (propertyInstance.getValue().equals(propertyInstance.getOldValue())){
-                        propertyInstance.setTicksSameValue(propertyInstance.getTicksSameValue() + 1);
-                    }else{
-                        propertyInstance.setOldValue(propertyInstance.getValue());
-                        propertyInstance.setTicksSameValue(0);
-                    }
+                if (termination.getCurrTick() % 100 == 0) {
+                    simulationOutcome.addSimulationForTickDTO(termination.getCurrTick(), simulationOutcome.getEntityInstanceManager().createDTO());
                 }
+                simulationOutcome.getEntityInstanceManager().killEntities();
+                simulationOutcome.getEntityInstanceManager().createEntities();
+                termination.setCurrTick(termination.getCurrTick() + 1);
+                if (simulationOutcome.isStop()) {
+                    break;
+                }
+                pauseAndResume();
             }
-            if(termination.getCurrTick() % 100 == 0) {
-                simulationOutcome.addSimulationForTickDTO(termination.getCurrTick(), simulationOutcome.getEntityInstanceManager().createDTO());
-            }
-            simulationOutcome.getEntityInstanceManager().killEntities();
-            simulationOutcome.getEntityInstanceManager().createEntities();
-            termination.setCurrTick(termination.getCurrTick()+1);
-            if(simulationOutcome.isStop()){
-                break;
-            }
-            pauseAndResume();
+        }catch (Exception e){
+            e.getMessage();
         }
 //        SimulationOutcome currSimulation = engine.getMyWorld().runSimulation(engine.getPropertyNameToValueAsString(),engine.getCountId());
 //        engine.setCountId(engine.getCountId() + 1);
 //        engine.getPastSimulations().put(engine.getCountId(), currSimulation);
     }
+
+    private static double getPopulation(Object value) {
+        double population;
+        if (value instanceof Integer) {
+            // Convert Integer to Double
+            population = ((Integer) value).doubleValue();
+        } else if (value instanceof Double) {
+            // Use the Double value directly
+            population = (Double) value;
+        } else {
+            // Handle other cases or provide a default value
+            population = 0.0; // Default value, change as needed
+        }
+        return population;
+    }
+
     private void moveEntities(){
         simulationOutcome.
                 getEntityInstanceManager().
